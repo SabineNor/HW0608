8dd8d722c0b8d74c63ee4ea95bd64747ec56826f
Sabine Nortone <sabineveidule@inbox.lv>
2020-08-09 15:03:07 +0300

    First commit - FoodCal

---
+++ src/main/scala/FoodCalculator.scala
@@ -0,0 +1,8 @@
+object FoodCalculator extends App {
+  val food = "potatoes"
+  val price = 0.37
+  val quantity = 20
+  val costs = price * quantity
+  println(s"It will cost $costs to buy $quantity kilos of $food")
+
+}
---
+++ src/main/scala/food.sc
@@ -0,0 +1,5 @@
+val food = "potatoes"
+val price = 0.37
+val quantity = 20
+val costs = price * quantity
+println(s"It will cost $costs to buy $quantity kilos of $food")
