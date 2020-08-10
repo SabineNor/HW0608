b80d431098d84006c71d986cba79c12b1d26c2a3
Sabine Nortone <sabineveidule@inbox.lv>
2020-08-10 19:16:10 +0300

    Made Food Calculator use 3 arguments

--- src/main/scala/FoodCalculator.scala
+++ src/main/scala/FoodCalculator.scala
@@ -2,7 +2,12 @@ object FoodCalculator extends App {
   val food = "potatoes"
   val price = 0.37
   val quantity = 20
-  val costs = price * quantity
-  println(s"It will cost $costs to buy $quantity kilos of $food")
+
+  println(s"We got ${args.length} arguments")
+  println(s"The first argument is ${args(0)}")
+  println(s"The second argument is ${args(1)}")
+  println(s"The third argument is ${args(2)}")
+  val costs = args(0).toFloat * args(1).toInt
+  println(s"It will cost $costs to buy ${args(1)} kilos of ${args(2)}")
 
 }
